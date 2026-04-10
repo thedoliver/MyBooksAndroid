b72f1dc012b5b6db27da9216b8ddc5eaf882e710
Denis <denisomachado@outlook.com>
2026-04-10 15:49:47 -0300

    feat: Implementado a tela de favoritos adicionado adapter metodos attacheListerner para ouvir a viewModel e observerse para retornar para viewmode para mudança de lista de favoritos vazia

--- app/src/main/java/com/example/mybooks/ui/FavoriteFragment.kt
+++ app/src/main/java/com/example/mybooks/ui/FavoriteFragment.kt
@@ -6,7 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mybooks.R
 import com.example.mybooks.databinding.FragmentFavoriteBinding
+import com.example.mybooks.helper.BookConstants
+import com.example.mybooks.ui.adapter.BookAdapter
+import com.example.mybooks.ui.listener.BookListerner
 import com.example.mybooks.viewmodel.FavoriteViewModel
 
 class FavoriteFragment : Fragment() {
@@ -16,9 +22,8 @@ class FavoriteFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
-
-    private val viewModel: FavoriteViewModel by viewModels() //Delegamos essa inicialização para biblioteca do android x
-
+    private val favoriteViewModel: FavoriteViewModel by viewModels() //Delegamos essa inicialização para biblioteca do android x
+    private val adapter: BookAdapter = BookAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,10 +34,52 @@ class FavoriteFragment : Fragment() {
         // removed para viewModels viewMode = ViewModelProvider(this).get(FavoriteViewModel::class.java)
 
         _binding = FragmentFavoriteBinding.inflate(inflater, container, false)
+        binding.recyclerviewFavorites.layoutManager = LinearLayoutManager(context)
+        binding.recyclerviewFavorites.adapter = adapter
 
-
+        attachListener()
+        setObservers()
 
         return binding.root
+    }
+
+    private fun attachListener(){
+        adapter.attachListener(object : BookListerner {
+            override fun onClick(id: Int) {
+
+                val bundle = Bundle()
+                bundle.putInt(BookConstants.KEY.BOOK_ID, id)
+
+                findNavController().navigate(R.id.navigation_details, bundle)
+            }
+
+            override fun onFavoriteClick(id: Int) {
+                favoriteViewModel.favorite(id)
+                favoriteViewModel.getFavoriteBooks()
+            }
+        })
+    }
+
+    override fun onResume() {
+        //Chamada dos livros
+        super.onResume()
+        favoriteViewModel.getFavoriteBooks()
+
+    }
+
+    private fun setObservers(){
+        favoriteViewModel.books.observe(viewLifecycleOwner) {
+            if (it.isEmpty()){
+                binding.recyclerviewFavorites.visibility = View.GONE
+                binding.textviewNoBooks.visibility = View.VISIBLE
+                binding.imageviewNoBooks.visibility = View.VISIBLE
+            }else {
+                binding.recyclerviewFavorites.visibility = View.VISIBLE
+                binding.textviewNoBooks.visibility = View.GONE
+                binding.imageviewNoBooks.visibility = View.GONE
+                adapter.updateList(it)
+            }
+        }
     }
 
     override fun onDestroyView() {
