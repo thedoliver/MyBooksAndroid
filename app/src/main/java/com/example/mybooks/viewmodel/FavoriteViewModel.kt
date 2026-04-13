1ac36382942fdf99b966e948e9a550d12704a0de
Denis <denisomachado@outlook.com>
2026-04-13 13:51:50 -0300

    feat: implementado o Singleton FavoriteViewModel na variable e injetado context no e mudado otipo de view para AndroidView que é compativel com singleton

--- app/src/main/java/com/example/mybooks/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/example/mybooks/viewmodel/FavoriteViewModel.kt
@@ -1,17 +1,19 @@
 package com.example.mybooks.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mybooks.entity.BookEntity
 import com.example.mybooks.repository.BookRepository
 
-class FavoriteViewModel() : ViewModel() {
+class FavoriteViewModel(application: Application) : AndroidViewModel(application) {
 
     private val _books = MutableLiveData<List<BookEntity>>()
     val books: LiveData<List<BookEntity>> = _books
 
-    private val repository = BookRepository.getInstance()
+    private val repository = BookRepository.getInstance(application.applicationContext)
 
 
     fun getFavoriteBooks() {
