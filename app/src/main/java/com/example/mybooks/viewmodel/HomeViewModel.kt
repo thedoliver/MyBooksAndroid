0dfe57b2c7f51194fb81e96868d8184905ab6163
Denis <denisomachado@outlook.com>
2026-04-13 13:52:10 -0300

    feat: implementado o Singleton HomeViewModel na variable e injetado context no e mudado otipo de view para AndroidView que é compativel com singleton

--- app/src/main/java/com/example/mybooks/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/mybooks/viewmodel/HomeViewModel.kt
@@ -1,20 +1,24 @@
 package com.example.mybooks.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.mybooks.entity.BookEntity
 import com.example.mybooks.repository.BookRepository
 
-class HomeViewModel : ViewModel() {
+class HomeViewModel(application: Application) : AndroidViewModel(application) {
+
+    //Variavel para acessar o repositorios
+    private val repository = BookRepository.getInstance(application.applicationContext)
 
     //Quando eu chamo a view model ele busca do repositorio reporna o _books e atribui a variavel books
     private val _books = MutableLiveData<List<BookEntity>>()
     //item observdo recebe
     val books: LiveData<List<BookEntity>> = _books
 
-    //Variavel para acessar o repositorios
-    private val repository = BookRepository.getInstance()
+
 
 
     fun getAllBooks(){
