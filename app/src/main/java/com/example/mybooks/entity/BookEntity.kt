d26edb4acf135ca8ef71289063fbbe935e759749
Denis <denisomachado@outlook.com>
2026-04-13 21:00:45 -0300

    refactor: implemented Room

--- app/src/main/java/com/example/mybooks/entity/BookEntity.kt
+++ app/src/main/java/com/example/mybooks/entity/BookEntity.kt
@@ -1,10 +1,27 @@
 package com.example.mybooks.entity
 
-data class BookEntity (
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Book")
+class BookEntity (
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
     val id: Int,
+
+    @ColumnInfo(name = "title")
     val title: String,
+
+    @ColumnInfo(name = "author")
     val author: String,
+
+
+    @ColumnInfo(name = "favorite")
     var favorite: Boolean,
+
+    @ColumnInfo(name = "genre")
     val genre: String,
 )
 
